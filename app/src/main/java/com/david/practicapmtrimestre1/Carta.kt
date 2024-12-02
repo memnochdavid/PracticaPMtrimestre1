acd7c10c70ac5835425049133f1ad97ba5223713
David Duque <davduquediaz@gmail.com>
2024-12-02 22:05:56 +0100

    jhvkhj

--- app/src/main/java/com/david/practicapmtrimestre1/Carta.kt
+++ app/src/main/java/com/david/practicapmtrimestre1/Carta.kt
@@ -1,8 +1,26 @@
 package com.david.practicapmtrimestre1
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 data class Carta(
     var numero: Int,
@@ -17,17 +35,44 @@ val baraja= mutableListOf<Carta>(
     Carta(3,R.drawable.chichibulma),
     Carta(4,R.drawable.launch),
     Carta(5,R.drawable.videl),
+    Carta(6,R.drawable.zangya),
+    Carta(1,R.drawable.bra),
+    Carta(2,R.drawable.cauliffla),
+    Carta(3,R.drawable.chichibulma),
+    Carta(4,R.drawable.launch),
+    Carta(5,R.drawable.videl),
     Carta(6,R.drawable.zangya)
 )
 
 @Composable
-fun MuestraCarta(carta: Carta){
-    if(carta.volteada){
-        Image(painter = painterResource(id = carta.img), contentDescription = "")
-    }else{
-        Image(painter = painterResource(id = carta.dorso), contentDescription = "")
+fun MuestraCarta(carta: Carta) {
+    var isFlipped by remember { mutableStateOf(carta.volteada) } // State to track flipped state
+
+    Image(
+        painter = painterResource(id = if (isFlipped) carta.img else carta.dorso),
+        contentDescription = "",
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(7.dp)
+            .clickable {
+                isFlipped = !isFlipped // Toggle flipped state on click
+                carta.volteada = !carta.volteada
+            }
+    )
+}
+
+@Composable
+fun MuestraBaraja() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(3),
+        modifier = Modifier.wrapContentSize()
+    ) {
+        items(mazoBarajado()) { carta ->
+            MuestraCarta(carta)
+        }
     }
 }
+
 
 fun mazoBarajado(): MutableList<Carta>{
     baraja.shuffle()
--- app/src/main/java/com/david/practicapmtrimestre1/MemoTron.kt
+++ app/src/main/java/com/david/practicapmtrimestre1/MemoTron.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.david.practicapmtrimestre1.ui.theme.PracticaPMtrimestre1Theme
 
 class MemoTron : ComponentActivity() {
@@ -19,29 +30,39 @@ class MemoTron : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticaPMtrimestre1Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                Mesa()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun Mesa() {
+    Row(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(colorResource(R.color.tapete)),
+        verticalAlignment = Alignment.CenterVertically,
+    ){
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(all = 20.dp),
+        ){
+            MuestraBaraja()
+        }
+        Row(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(all = 20.dp),
+        ){
+            Text("")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    PracticaPMtrimestre1Theme {
-        Greeting("Android")
-    }
+    Mesa()
 }
