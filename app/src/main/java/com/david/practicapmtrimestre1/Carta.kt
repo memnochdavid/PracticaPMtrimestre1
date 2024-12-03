2809aa13146de875ed61f2eb92bc2da5bdbba9d7
memnochdavid <davduquediaz@gmail.com>
2024-12-03 18:14:21 +0100

    xfgmnxfghm

--- app/src/main/java/com/david/practicapmtrimestre1/Carta.kt
+++ app/src/main/java/com/david/practicapmtrimestre1/Carta.kt
@@ -1,5 +1,7 @@
 package com.david.practicapmtrimestre1
 
+import android.util.Log
+import androidx.activity.result.launch
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.padding
@@ -13,17 +15,23 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 data class Carta(
     var numero: Int,
     var img: Int,
     val dorso: Int =R.drawable.dorso,
-)
+){
+    override fun toString(): String {
+        return "Carta(numero=$numero, img=$img)"
+    }
+}
 //global
 
 val baraja= mutableListOf<Carta>(
@@ -40,31 +48,54 @@ val baraja= mutableListOf<Carta>(
     Carta(5,R.drawable.videl),
     Carta(6,R.drawable.zangya)
 )
-var volteadas = MutableList(12) { false }
+
 
 @Composable
 fun MuestraCarta(carta: Carta, index: Int) {
-    var volteada by remember { mutableStateOf(volteadas[index]) }
+    var seleccionada by remember { mutableStateOf<Carta?>(null) }
+    val lifecycleScope = rememberCoroutineScope()
+    var turno=0
+    LaunchedEffect(key1 = vidas) {
+        delay(500)
+        if (volteadas.isNotEmpty()) {
+            volteadas.forEachIndexed { i, _ ->
+                volteadas[i] = false
+            }
+        }
+    }
 
     Image(
-        painter = painterResource(id = if (volteada) carta.img else carta.dorso),
+        painter = painterResource(id = if (volteadas[index]) carta.img else carta.dorso),
         contentDescription = "",
         modifier = Modifier
             .size(130.dp)
             .padding(7.dp)
             .clickable {
-                if (!volteadas[index]) {
-                    volteadas[index] = true
-                    volteada=true
-
-                }
-
+                //lifecycleScope.launch {
+                    //if (!volteadas[index]) {//si no esta volteada
+                        if (seleccionada == null) {
+                            seleccionada = carta
+                            volteadas[index] = true
+                        } else {
+                            if (seleccionada!!.numero == carta.numero) {
+                                volteadas[index] = true
+                                seleccionada = null
+                            }
+                            else {
+                                volteadas[index] = true
+                                seleccionada = null
+                                //delay(300)
+                                vidas--
+                            }
+                        }
+                    //}
+                //}
             }
     )
 }
+
 @Composable
 fun MuestraBaraja(mazo: List<Carta>) {
-
 
     LazyVerticalGrid(
         columns = GridCells.Fixed(3),
--- app/src/main/java/com/david/practicapmtrimestre1/MemoTron.kt
+++ app/src/main/java/com/david/practicapmtrimestre1/MemoTron.kt
@@ -5,17 +5,24 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -23,8 +30,10 @@ import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import com.david.practicapmtrimestre1.ui.theme.PracticaPMtrimestre1Theme
 
-var vidas by mutableStateOf<Int>(5)
+var vidas by mutableIntStateOf(5)
 var mazo by mutableStateOf(mutableListOf<Carta>())
+//var volteadas = MutableList(12) { false }
+var volteadas = mutableStateListOf(false, false, false, false, false, false, false, false, false, false, false, false)
 
 
 class MemoTron : ComponentActivity() {
@@ -41,6 +50,9 @@ class MemoTron : ComponentActivity() {
 
 @Composable
 fun Mesa() {
+    //var volteadas by remember{mutableStateOf(mutableListOf<Boolean>(false,false,false,false,false,false,false,false,false,false,false,false))}
+
+
     ConstraintLayout(
         modifier = Modifier
             .fillMaxSize()
@@ -66,16 +78,37 @@ fun Mesa() {
                 .constrainAs(fila2) {
                     top.linkTo(fila1.bottom)
                     bottom.linkTo(parent.bottom)
-                }
-        ){
-            Text("Vidas: $vidas")
-            Button(onClick = {
-                vidas=5
-                mazo=mazoBarajado().toMutableList()
-            }) {
-                Text("Comenzar")
+                },
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically
 
+        ){
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .weight(0.5f)
+            ) {
+                Text("Vidas: $vidas")
             }
+            Column(
+                modifier = Modifier
+                    .wrapContentSize()
+                    .weight(0.5f)
+            ) {
+                Button(onClick = {
+                    vidas=5
+                    volteadas.forEachIndexed { index, _ ->
+                        if(volteadas.size>0){
+                            volteadas[index] = false
+                        }
+                    }
+                    mazo=mazoBarajado().toMutableList()
+                }) {
+                    Text("Comenzar")
+
+                }
+            }
+
         }
     }
 }
